e3877d3c9df7729e2c8868e818626cbe8e178b26
Marina <for-marina@mail.ru>
2021-03-12 12:26:23 +0100

    feat: implement brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const open = new Set(bracketsConfig.map(pair => pair[0]));
+  const close  = new Set(bracketsConfig.map(pair => pair[1]));
+  const pairs = bracketsConfig.reduce((acc, [open, close]) => ({...acc, [close]: open}), {});
+  let stack = []
+
+ for(const ch of str)
+  {
+      if(open.has(ch) && close.has(ch))
+      {
+        if(stack.length === 0)
+        {
+          stack.push(ch);
+        }
+        else
+        {
+          let prev = stack.pop();
+          if(prev !== ch)
+          {
+            stack.push(prev); 
+            stack.push(ch);     
+          }
+          else
+          {
+            if (pairs[ch] !== prev) return false;
+          }
+        }
+      }
+      else
+      {
+      if (open.has(ch)) {
+          stack.push(ch);
+      }
+      if (close.has(ch)) {
+        if (pairs[ch] !== stack.pop()) return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
